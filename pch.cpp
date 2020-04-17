1bc20ec95d643e01bf83cbf86fbfabcfdac81984
wangyff-code <609572929@qq.com>
2020-04-17 14:13:53 +0800

    dll_update

--- pch.cpp
+++ pch.cpp
@@ -1,8 +1,7 @@
 ﻿// pch.cpp: 与预编译标头对应的源文件
 
 #include "pch.h"
-#include"stdio.h"
-
+#include "immintrin.h"
 // 当使用预编译的头时，需要使用此源文件，编译才能成功。
 
 typedef struct {
@@ -11,12 +10,6 @@ typedef struct {
 	unsigned char r;
 }color_type;
 
-typedef struct {
-	unsigned char b;
-	unsigned char g;
-	unsigned char r;
-	unsigned char v;
-}list_type;
 
 typedef struct {
 	long pix_len;
@@ -25,38 +18,36 @@ typedef struct {
 
 
 
-long dis_tens_color(color_type* c1, color_type* c2,unsigned char v)
+long dis_tens_color(color_type* c1, color_type* c2)
 {
 	float temp = 0;
 	temp += ((int)c1->b - (int)c2->b) * ((int)c1->b - (int)c2->b);
 	temp += ((int)c1->g - (int)c2->g) * ((int)c1->g - (int)c2->g);
 	temp += ((int)c1->r - (int)c2->r) * ((int)c1->r - (int)c2->r);
-	temp *= (float)v / 50.0;
 	return temp;
 }
 
-int change_color(color_type* c1, list_type* listPtr, int list_len)
+int change_color(color_type* c1, color_type* listPtr, int list_len)
 {
 	long temp = 0;
 	long temp_min = 0;
 	unsigned char item_id = 0;
-	color_type* list_temp = (color_type*)(listPtr);
+	color_type* list_temp = listPtr;
 	int i;
 	for (i = 0; i < list_len; i++)
 	{
+		temp = dis_tens_color(c1,listPtr + i);
 		if (i == 0)
 		{
-			temp = dis_tens_color(c1, (color_type*)(listPtr + i), (listPtr + i)->v);
 			temp_min = temp;
-			list_temp = (color_type*)(listPtr + i);
+			list_temp = listPtr + i;
 			item_id = i;
 		}
 		else
 		{
-			temp = dis_tens_color(c1, (color_type*)(listPtr + i), (listPtr + i)->v);
 			if (temp < temp_min)
 			{
-				list_temp = (color_type*)(listPtr + i);
+				list_temp = listPtr + i;
 				temp_min = temp;
 				item_id = i;
 			}
@@ -69,8 +60,6 @@ int change_color(color_type* c1, list_type* listPtr, int list_len)
 }
 
 
-
-
 int img_CV(unsigned char* imgPtr,unsigned char * listPtr,unsigned char * pix_ptr,unsigned char * arg_ptr)
 {
 	arg_type * arg =(arg_type*)arg_ptr;
@@ -78,7 +67,7 @@ int img_CV(unsigned char* imgPtr,unsigned char * listPtr,unsigned char * pix_ptr
 	unsigned char item_id = 0;
 	for (pix_number = 0; pix_number < arg->pix_len; pix_number++)
 	{
-		item_id=change_color((color_type*)(imgPtr)+pix_number,(list_type*)listPtr, arg->list_len);
+		item_id=change_color((color_type*)(imgPtr)+pix_number,(color_type*)listPtr, arg->list_len);
 		*(pix_ptr + pix_number) = item_id;
 	}
 	return 0;
